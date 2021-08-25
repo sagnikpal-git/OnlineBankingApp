030c2eb32f65eb6991fc9fc782f3b1f8e27ec0ad
sagnikpal-git <70804604+sagnikpal-git@users.noreply.github.com>
2021-08-25 19:59:19 +0530

    update2

--- src/main/java/com/online/banking/app/controller/FundTransferController.java
+++ src/main/java/com/online/banking/app/controller/FundTransferController.java
@@ -17,6 +17,8 @@ import com.online.banking.app.model.Response;
 import com.online.banking.app.model.TransferBalanceRequest;
 import com.online.banking.app.service.FundTransferringService;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 public class FundTransferController {
 
@@ -24,12 +26,15 @@ public class FundTransferController {
 	FundTransferringService service;
 
 	@RequestMapping(value = "/create-account-info", method = RequestMethod.POST)
+	@ApiOperation(value = "Create bank account info", notes = "Generate bank account information")
+	
 	public ResponseEntity<BankAccountInfo> create(@RequestBody BankAccountInfo accountinfo) {
 		BankAccountInfo BAI = service.save(accountinfo);
 		return ResponseEntity.status(HttpStatus.CREATED).body(BAI);
 	}
 
 	@RequestMapping(value = "/get-all-bank-details", method = RequestMethod.GET)
+	@ApiOperation(value = "Get all bank account details", notes = "Get all account details in bank database")
 
 	public ResponseEntity<List<BankAccountInfo>> getAllAccountDetails() {
 		List<BankAccountInfo> getaccounts = service.getAllAccounts();
@@ -37,12 +42,11 @@ public class FundTransferController {
 	}
 
 	@PutMapping("/send-money")
+	@ApiOperation(value = "Transfer funds between accounts", notes = "Send money to beneficiary account")
 	public ResponseEntity<ResponseEntity<Response>> SendMoney(
 			@RequestBody TransferBalanceRequest transferBalanceRequest) throws UnableToTransferFundException {
 
 		ResponseEntity<Response> transfer = service.TransferBalanceRequest(transferBalanceRequest);
 		return ResponseEntity.status(HttpStatus.OK).body(transfer);
-
 	}
-
 }
