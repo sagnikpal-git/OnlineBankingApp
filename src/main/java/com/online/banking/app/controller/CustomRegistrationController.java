01a39c15b28549fcf25126c167ea6f82c4b29674
sagnikpal-git <70804604+sagnikpal-git@users.noreply.github.com>
2021-08-25 19:58:22 +0530

    update2

--- src/main/java/com/online/banking/app/controller/CustomRegistrationController.java
+++ src/main/java/com/online/banking/app/controller/CustomRegistrationController.java
@@ -16,42 +16,78 @@ import com.online.banking.app.exceptions.IdNotFoundException;
 import com.online.banking.app.model.CustomerRegistrationInfo;
 import com.online.banking.app.service.CustomerRegistrationService;
 
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 public class CustomRegistrationController {
 	@Autowired
 	CustomerRegistrationService service;
 
-	@RequestMapping(value="/get-customer/{userId}", method=RequestMethod.GET)
+	@RequestMapping(value = "/get-customer/{userId}", method = RequestMethod.GET)
+	@ApiOperation(value = "Get customer by Id", notes = "Find account details by user id")
+
 	public ResponseEntity<CustomerRegistrationInfo> GetByUserId(@PathVariable Integer userId)
-			throws IdNotFoundException{
-		CustomerRegistrationInfo customerinfo=service.getByuserId(userId);
-		if(customerinfo==null)
-			throw new IdNotFoundException("Customer not found with the given Id::" +userId);
-		return ResponseEntity.status(HttpStatus.OK).body(customerinfo);		
+			throws IdNotFoundException {
+		CustomerRegistrationInfo customerinfo = service.getByuserId(userId);
+		if (customerinfo == null)
+			throw new IdNotFoundException("Customer not found with the given Id::" + userId);
+		return ResponseEntity.status(HttpStatus.OK).body(customerinfo);
 	}
-	
-	@RequestMapping(value="/get-customerby-uidai/{aadharNumber}", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/get-customerby-uidai/{aadharNumber}", method = RequestMethod.GET)
+	@ApiOperation(value = "Get customer by uidai", notes = "Find account details with aadhar number")
+
 	public ResponseEntity<CustomerRegistrationInfo> GetByAadharNumber(@PathVariable Long aadharNumber)
-			throws AadharNotFoundException{
-		
-		CustomerRegistrationInfo customerinfo=service.getByAadharNumber(aadharNumber);
-		if(customerinfo==null)
-			throw new AadharNotFoundException("Customer not found with Aadhar Number::" +aadharNumber);
-		return ResponseEntity.status(HttpStatus.OK).body(customerinfo);		
+			throws AadharNotFoundException {
+
+		CustomerRegistrationInfo customerinfo = service.getByAadharNumber(aadharNumber);
+		if (customerinfo == null)
+			throw new AadharNotFoundException("Customer not found with Aadhar Number::" + aadharNumber);
+		return ResponseEntity.status(HttpStatus.OK).body(customerinfo);
 	}
-	
-	@RequestMapping(value="/create-customer", method=RequestMethod.POST)
-	public ResponseEntity<CustomerRegistrationInfo> CreateCustomerInfo
-	(@RequestBody CustomerRegistrationInfo customerregistartioninfo){
-		CustomerRegistrationInfo customerinfo=service.CreateCustomerInfo(customerregistartioninfo);
-		return ResponseEntity.status(HttpStatus.CREATED).body(customerinfo);		
+
+	@RequestMapping(value = "/create-customer", method = RequestMethod.POST)
+	@ApiOperation(value = "Add a new account", notes = "Create a new account for customer")
+
+	public ResponseEntity<CustomerRegistrationInfo> CreateCustomerInfo(
+			@RequestBody CustomerRegistrationInfo customerregistartioninfo) {
+		CustomerRegistrationInfo customerinfo = service.CreateCustomerInfo(customerregistartioninfo);
+		return ResponseEntity.status(HttpStatus.CREATED).body(customerinfo);
 	}
-	
-	@RequestMapping(value="/get-all-customers", method=RequestMethod.GET)
-	public ResponseEntity<List<CustomerRegistrationInfo>> GetAllCutomers(){
-		List<CustomerRegistrationInfo> customerinfo=service.GetAllCustomers();
-				return ResponseEntity.status(HttpStatus.OK).body(customerinfo);		
-	}	
-	
+
+	@RequestMapping(value = "/get-all-customers", method = RequestMethod.GET)
+	@ApiOperation(value = "get all customers", notes = "Find account details of all customers")
+
+	public ResponseEntity<List<CustomerRegistrationInfo>> GetAllCutomers() {
+		List<CustomerRegistrationInfo> customerinfo = service.GetAllCustomers();
+		return ResponseEntity.status(HttpStatus.OK).body(customerinfo);
+	}
+
+	@RequestMapping(value = "/update-customer/{userId}", method = RequestMethod.PUT)
+	@ApiOperation(value = "Update customer details", notes = "Update customer information with user id in database")
+
+	public ResponseEntity<String> UpdateCustomerInfo(@PathVariable Integer userId,
+			@RequestBody CustomerRegistrationInfo user) throws IdNotFoundException {
+
+		CustomerRegistrationInfo customerinfo = service.getByuserId(userId);
+		if (customerinfo.getUserId() == null)
+			throw new IdNotFoundException("Customer account not found with userid::" + userId);
+
+		customerinfo.setAddress(user.getAddress());
+		customerinfo.setAadharNumber(user.getAadharNumber());
+		customerinfo.setBankAccountInfo(user.getBankAccountInfo());
+		customerinfo.setEmailId(user.getEmailId());
+		customerinfo.setMobileNumber(user.getMobileNumber());
+		customerinfo.setName(user.getName());
+		CustomerRegistrationInfo updatedetails = service.UpdateCustomerInfo(customerinfo);
+		return new ResponseEntity<String>(HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/delete-customer-account/{userId}", method = RequestMethod.DELETE)
+	@ApiOperation(value = "Delete customer and related accounts", notes = "Delete customer and account details associated with him")
+
+	public HttpStatus deleteProduct(@PathVariable Integer userId) {
+		service.DeleteCustomerInfo(userId);
+		return HttpStatus.OK;
+	}
 }
